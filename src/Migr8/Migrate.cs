ef63edb9d27984514abdc0b676a209d53ed2cd67
Mogens Heller Grabe <mookid8000@gmail.com>
2012-07-05 08:01:49 +0200

    started implementing a nifty execution API

---
+++ src/Migr8/Migrate.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Migr8
+{
+    public class Migrate
+    {
+        /// <summary>
+        /// Picks up migrations from the calling assembly and executes them against the database specified by the given connection string.
+        /// </summary>
+        public static void Database(string connectionString)
+        {
+            var callingAssembly = Assembly.GetCallingAssembly();
+            Execute(connectionString, callingAssembly);
+        }
+
+        static void Execute(string connectionString, Assembly callingAssembly)
+        {
+            using (var migrate = new DatabaseMigrator(connectionString, new AssemblyScanner(callingAssembly)))
+            {
+                migrate.MigrateDatabase();
+            }
+        }
+    }
+}
