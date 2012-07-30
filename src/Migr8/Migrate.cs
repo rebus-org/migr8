6a191003713a82eabc2a561d9cb7f61b4ea85b4d
Mogens Heller Grabe <mookid8000@gmail.com>
2012-07-30 13:44:31 +0200

    more consistent naming

--- src/Migr8/Migrate.cs
+++ src/Migr8/Migrate.cs
@@ -11,7 +11,7 @@ namespace Migr8
         /// The connection string can be specified either by providing a key of a connection string in the connectionStrings configuration
         /// section of your app.config/web.config, or by providing the raw connection string.
         /// </summary>
-        public static void Database(string connectionStringOrConnectionStringKey, Action<IMigration> beforeExecute = null, Action<IMigration> afterExecute = null, Action<IMigration, Exception> afterExecuteError = null)
+        public static void Database(string connectionStringOrConnectionStringKey, Action<IMigration> beforeExecute = null, Action<IMigration> afterExecuteSuccess = null, Action<IMigration, Exception> afterExecuteError = null)
         {
             var connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringOrConnectionStringKey];
 
@@ -21,15 +21,15 @@ namespace Migr8
 
             var callingAssembly = Assembly.GetCallingAssembly();
 
-            Execute(connectionString, callingAssembly, beforeExecute, afterExecute, afterExecuteError);
+            Execute(connectionString, callingAssembly, beforeExecute, afterExecuteSuccess, afterExecuteError);
         }
 
-        static void Execute(string connectionString, Assembly callingAssembly, Action<IMigration> beforeExecute, Action<IMigration> afterExecute, Action<IMigration, Exception> afterExecuteError)
+        static void Execute(string connectionString, Assembly callingAssembly, Action<IMigration> beforeExecute, Action<IMigration> afterExecuteSuccess, Action<IMigration, Exception> afterExecuteError)
         {
             using (var migrate = new DatabaseMigrator(connectionString, new AssemblyScanner(callingAssembly)))
             {
                 if (beforeExecute != null) migrate.BeforeExecute += beforeExecute;
-                if (afterExecute != null) migrate.AfterExecuteSuccess += afterExecute;
+                if (afterExecuteSuccess != null) migrate.AfterExecuteSuccess += afterExecuteSuccess;
                 if (afterExecuteError != null) migrate.AfterExecuteError += afterExecuteError;
 
                 migrate.MigrateDatabase();
