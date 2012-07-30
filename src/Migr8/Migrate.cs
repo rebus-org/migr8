4a32bdae4c66b89b045176f52d8665ff80c5029d
Mogens Heller Grabe <mookid8000@gmail.com>
2012-07-30 13:42:14 +0200

    added ability to subscribe to events from the simple main API

--- src/Migr8/Migrate.cs
+++ src/Migr8/Migrate.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Reflection;
 
 namespace Migr8
@@ -10,7 +11,7 @@ namespace Migr8
         /// The connection string can be specified either by providing a key of a connection string in the connectionStrings configuration
         /// section of your app.config/web.config, or by providing the raw connection string.
         /// </summary>
-        public static void Database(string connectionStringOrConnectionStringKey)
+        public static void Database(string connectionStringOrConnectionStringKey, Action<IMigration> beforeExecute = null, Action<IMigration> afterExecute = null, Action<IMigration, Exception> afterExecuteError = null)
         {
             var connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringOrConnectionStringKey];
 
@@ -19,14 +20,18 @@ namespace Migr8
                                        : connectionStringOrConnectionStringKey;
 
             var callingAssembly = Assembly.GetCallingAssembly();
-            
-            Execute(connectionString, callingAssembly);
+
+            Execute(connectionString, callingAssembly, beforeExecute, afterExecute, afterExecuteError);
         }
 
-        static void Execute(string connectionString, Assembly callingAssembly)
+        static void Execute(string connectionString, Assembly callingAssembly, Action<IMigration> beforeExecute, Action<IMigration> afterExecute, Action<IMigration, Exception> afterExecuteError)
         {
             using (var migrate = new DatabaseMigrator(connectionString, new AssemblyScanner(callingAssembly)))
             {
+                if (beforeExecute != null) migrate.BeforeExecute += beforeExecute;
+                if (afterExecute != null) migrate.AfterExecuteSuccess += afterExecute;
+                if (afterExecuteError != null) migrate.AfterExecuteError += afterExecuteError;
+
                 migrate.MigrateDatabase();
             }
         }
